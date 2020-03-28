4e020fa5838a18f249be4138a598f048bbb5fca7
Takahiro <tk.iyei@gmail.com>
2020-03-28 09:37:49 +0900

    firebase設定を追加

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+}
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
